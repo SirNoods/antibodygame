b5081f8335edba9e57ef536fd906533efc6f73ee
NoisyNoodle <42236284+SirNoods@users.noreply.github.com>
2025-01-23 22:52:07 +0100

    fixed the shooty bits
    no explanation necessary

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,23 +11,38 @@ const BOOST_DURATION = 0.3  # Dash lasts for 0.3 seconds
 # Variables
 var is_boosting = false
 var boost_timer = 0.0
+var bullet_speed = 1000
 
-# instances
-var projectile_scene = preload("res://scenes/basic_projectile.tscn")
+# Scene preloading
+var bullet = preload("res://scenes/bullet.tscn")
 
 func _ready() -> void:
-	print("Something")
+	print("Player loaded in")
+	assert(bullet is PackedScene)
+
+func fire():
+	# Create an instance of the bullet
+	var bullet_instance = bullet.instantiate()
+	
+	# Set the bullet's global position and rotation
+	bullet_instance.position = position  # position NOT global position??
+	bullet_instance.rotation = rotation  # Use the player's rotation
+	
+	bullet_instance.linear_velocity = Vector2(bullet_speed, 0).rotated(rotation)
+	
+	# Add the bullet to the same parent as the player
+	get_parent().add_child(bullet_instance)
+
 
 func _physics_process(delta):
+	look_at(get_global_mouse_position())
 	# Start with a consistent forward speed
 	velocity.x = FLOW_SPEED
-
+	
 	# Vertical movement (W/S or Up/Down keys)
 	if Input.is_action_pressed("move_up"):
-		print("up")
 		velocity.y = -VERTICAL_SPEED
 	elif Input.is_action_pressed("move_down"):
-		print("down")
 		velocity.y = VERTICAL_SPEED
 	else:
 		# Apply drag to vertical movement for a smooth, fluid feel
@@ -35,10 +50,8 @@ func _physics_process(delta):
 
 	# Horizontal adjustments (A/D or Left/Right keys)
 	if Input.is_action_pressed("move_left"):
-		print("left")
 		velocity.x -= HORIZONTAL_ADJUST_SPEED
 	elif Input.is_action_pressed("move_right"):
-		print("right")
 		velocity.x += HORIZONTAL_ADJUST_SPEED
 
 	# Boost mechanic (Spacebar)
@@ -46,7 +59,11 @@ func _physics_process(delta):
 		is_boosting = true
 		boost_timer = BOOST_DURATION
 		velocity.x = BOOST_SPEED
-
+	
+	if Input.is_action_just_pressed("shoot"):
+		print("pew")
+		fire()
+	
 	if is_boosting:
 		boost_timer -= delta
 		if boost_timer <= 0:
