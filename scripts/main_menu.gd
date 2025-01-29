b70f767662a3146e75a9a6766c56e742b160000c
Goshva <42236284+SirNoods@users.noreply.github.com>
2025-01-29 11:44:48 +0100

    added basic option menu
    without any options

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -6,17 +6,26 @@ extends Control
 @onready var options_button = $MarginContainer/HBoxContainer/VBoxContainer/options_button
 @onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/exit_button
 @onready var start_level = preload("res://scenes/game.tscn") as PackedScene
+@onready var options_menu = $options_menu as OptionsMenu
 
 func _ready():
-	pass
-
+	handle_connecting_signals()
 
 func _on_start_button_button_down() -> void:
-	print("fuck you I'm not starting yet") # Replace with function body.
+	print("Starting Game")
 	get_tree().change_scene_to_packed(start_level)
 
 func _on_options_button_button_down() -> void:
-	print("fuck your chicken strips")
+	print("Entering Options")
+	$MarginContainer.visible = false
+	$options_menu.visible = true
 
 func _on_exit_button_button_down() -> void:
 	get_tree().quit()
+	
+func _on_exit_options_menu() -> void:
+	$MarginContainer.visible = true
+	$options_menu.visible = false
+	
+func handle_connecting_signals() -> void:
+	options_menu.exit_options_menu.connect(_on_exit_options_menu)
